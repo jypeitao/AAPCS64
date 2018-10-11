2bd5780c00190cca557947c2b0eac42aaae4e4ab
tao.pei <tao.pei@ck-telecom.com>
2018-10-11 17:18:50 +0800

    m p

--- test_arm64.c
+++ test_arm64.c
@@ -3,7 +3,8 @@ int testb(int a) {
     int x = a -1;
     return x;
 }
-int testa(int a,int b) {
+int testa(int a,int b, int c1,int c2,int c3,int c4,int c5,int c6,int c7, int c8,int c9) {
+//int testa(int a,int b) {
     int x = a + b;
 
     int k[5]={1,2,3,4,5};
@@ -16,7 +17,7 @@ int testc(int a[]) {
 }
 int main() {
     printf("aapcs64\n");
-    int a = testa(1,3);
+    int a = testa(1,3,1,2,3,4,5,6,7,8,9);
     printf("a = %d\n",a);
     int k[5]={1,2,3,4,5};
     testc(k);
